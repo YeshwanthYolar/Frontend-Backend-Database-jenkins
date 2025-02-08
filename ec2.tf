d2599d41ee943c23fe9edbf7a60b36265aef9f6e
YeshwanthYolar <yolaryeshwanth@gmail.com>
2025-02-08 12:20:14 +0530

    removed provisioners and replaced with git clone

--- ec2.tf
+++ ec2.tf
@@ -6,19 +6,7 @@ resource "aws_instance" "public_instance" {
   key_name = "demo-project"
   vpc_security_group_ids = [aws_security_group.allow_all.id]
 
-
-# Provisioner to copy a local file to the EC2 instance
-  provisioner "file" {
-    source      = "./files/"         # Path to the local file on your machine
-    destination = "/home/ubuntu/"  # Destination path on the EC2 instance
-    connection {
-      type        = "ssh"
-      user        = "ubuntu"                  # SSH user (depends on your AMI, e.g., "ec2-user" for Amazon Linux)
-      private_key = file("./demo-project.pem")  # Path to your private key for SSH authentication
-      host        = aws_instance.public_instance.public_ip
-    }
-  }
-  depends_on = [ aws_instance.private_instance ]
+  depends_on = [ aws_instance.private_instance, aws_internet_gateway.demo-project-igw ]
 
   user_data = templatefile("frontend.sh", {
     private_ip = aws_instance.private_instance.private_ip
